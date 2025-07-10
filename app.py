9727ccc267a359fcb8bd8530ae1681ab6e5b3cfc
unknown <Aayusi>
2025-07-10 14:46:24 -0400

    word correction

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import joblib
 
+#Loads the TF-IDF vectorizer 
+#Loads the Logistic Regression model 
+
 vectorizer = joblib.load("vectorizer.joblib")
 model = joblib.load("lr_model.joblib")
 
+#Displays a large title on the web app
 st.title("Fake News Detector")
+
+#Adds some instructional text 
 st.write("Enter a News Article below to check whether it is Fake or Real. ")
 
+#cteate multiline text box
 inputn = st.text_area("News Article:","")
 
 if st.button("Check News"):
 
     if inputn.strip():
+        #Checks whether the user actually entered something
         transform_input = vectorizer.transform([inputn])
         prediction = model.predict(transform_input)
 
